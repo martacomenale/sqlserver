5b6f1a4ccf52fa7124ffa279bb071a628aabf8e6
martacomenale <martacomenale@gmail.com>
2024-11-22 21:21:46 -0300

    Create criar_banco.sql

---
+++ criar_banco.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE Base_Teste
+ON (NAME = 'base_de_dados_teste.DAT',
+        FILENAME = 'C:\TEMP\base_de_dados_teste.MDF',
+        SIZE = 10MB,
+        MAXSIZE = 50MB,
+        FILEGROWTH = 5MB)
